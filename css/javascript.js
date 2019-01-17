eb446c85db2fb306883ab4673eafe0eb6cb6d737
Francisco Prieto <panchoprieto01@gmail.com>
2019-01-17 23:02:28 +0000

    Day two

---
+++ css/javascript.js
@@ -0,0 +1,19 @@
+// JavaScript File
+
+
+// 1 and 2
+//var watchList = ["Rising of the Shield Hero, ", "FRIENDS, ", "One Punch Man, ", "Halo TV Show."];
+
+//$('.button').click(function() {
+// watchList.forEach(function(one){
+//     $(".watchList").append("<li>I Want to watch" + " " + one + "</li>");
+// });
+//});
+
+
+var songList = ["test"];
+('button').click(function() {
+songList.forEach(function(test){
+    (".songList").append(test);
+});
+});
