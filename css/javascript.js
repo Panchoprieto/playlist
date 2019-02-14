8b2160c628b70371e76f2c9c6a3e756e55c80745
Francisco Prieto <panchoprieto01@gmail.com>
2019-02-14 21:46:32 +0000

    done 2

--- css/javascript.js
+++ css/javascript.js
@@ -2,39 +2,19 @@
  
 /* global $ */
 
-var songs = [
-    "Rise",
-    "Bye Bye YESTERDAY",
-    "Ievan Polkka", 
-    "World is Mine"
+var songs = [ "Rise", "Bye Bye YESTERDAY", "Ievan Polkka", "World is Mine"
 ];
 
-var artists = [
-    "MADKID",
-    "3-nen E-gumi Utatan",
-    "Hatsune Miku", 
-    "Hatsune Miku"
+var artists = ["MADKID", "3-nen E-gumi Utatan", "Hatsune Miku", "Hatsune Miku"
 ];
 
-var images = [
-    "https://i.ytimg.com/an_webp/RhWKMHA7YWc/mqdefault_6s.webp?du=3000&sqp=COqaluMF&rs=AOn4CLAuOjkuMD8sEFKjwg-BPXT84E5nOg",
-    "https://i.ytimg.com/vi/FyS928U69No/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLCWlqvQpQkjyNtCN179AEg_sz_a1A",
-    "https://i.ytimg.com/an_webp/ZjDZrReZ4EI/mqdefault_6s.webp?du=3000&sqp=CK7rleMF&rs=AOn4CLCi_eh98tFl1UYkiEWOqzPcdMnppA",
-    "https://i.ytimg.com/vi/NY__VTIUsiU/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLCCdxYL5ahpJbUVEoSdGH69ok1QMQ"
+var images = ["https://i.ytimg.com/an_webp/RhWKMHA7YWc/mqdefault_6s.webp?du=3000&sqp=COqaluMF&rs=AOn4CLAuOjkuMD8sEFKjwg-BPXT84E5nOg", "https://i.ytimg.com/vi/FyS928U69No/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLCWlqvQpQkjyNtCN179AEg_sz_a1A", "https://i.ytimg.com/an_webp/ZjDZrReZ4EI/mqdefault_6s.webp?du=3000&sqp=CK7rleMF&rs=AOn4CLCi_eh98tFl1UYkiEWOqzPcdMnppA", "https://i.ytimg.com/vi/NY__VTIUsiU/hqdefault.jpg?sqp=-oaymwEYCKgBEF5IVfKriqkDCwgBFQAAiEIYAXAB&rs=AOn4CLCCdxYL5ahpJbUVEoSdGH69ok1QMQ"
 ];
 
-var links = [
-    "https://www.youtube.com/watch?v=RhWKMHA7YWc&t=23s",
-    "https://www.youtube.com/watch?v=FyS928U69No",
-    "https://www.youtube.com/watch?v=ZjDZrReZ4EI",
-    "https://www.youtube.com/watch?v=NY__VTIUsiU"
+var links = ["https://www.youtube.com/watch?v=RhWKMHA7YWc&t=23s", "https://www.youtube.com/watch?v=FyS928U69No", "https://www.youtube.com/watch?v=ZjDZrReZ4EI", "https://www.youtube.com/watch?v=NY__VTIUsiU"
 ];
 
-var lengths = [
-    "3:36",
-    "4:09",
-    "2:30",
-    "4:16"
+var lengths = ["3:36", "4:09", "2:30", "4:16"
 ];
 
 function displaySongInfo(){
